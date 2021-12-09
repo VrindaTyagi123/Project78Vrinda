a80dd0e250f2e08726339e91c789fa41729a4586
VrindaTyagi123 <83289238+VrindaTyagi123@users.noreply.github.com>
2021-12-09 13:57:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-    
+var family_member_name=[
+    "2. My Father (Abhishek Tyagi)",
+    "3. My Mother (Shivani Tyagi)",
+    "4. Me (Vrinda Tyagi)"
+  ];
+
+  var family_member_image=[
+    "unnamed.jpg",
+    "unnamed (1).jpg",
+    "unnamed (2).jpg"
+  ];
+
+ var i=0;
+
+ function update()
+ {
+
+    i++;
+    var numbers_of_family_member_in_array = 3
+    if(i > numbers_of_family_member_in_array)
+    {
+        i = 0;
+    }
+
+    var updatedImage = images[i];
+    var updateName = names[i];
+    document.getElementById("family_member_image").src= updatedImage;
+    document.getElementById("family_member_name").innerHTML = updateName;
+ }
